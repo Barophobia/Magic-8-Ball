8b0125467b2fe3da48b7dc55e6c4eccb3b122c78
Barophobia <georgerosephone@gmail.com>
2019-12-25 19:13:32 +0000

    Create Magic8Ball.py

---
+++ Magic8Ball.py
@@ -0,0 +1,72 @@
+import random
+
+question = input("What would you like to ask the magic 8 ball? ")
+
+magic = random.randint(1,20)
+
+if magic == 1:
+    print("Outlook not so good")
+
+if magic == 2:
+    print("Yes.")
+
+if magic == 3:
+    print("You may rely on it.")
+
+if magic == 4:
+    print("As I see it, yes.")
+
+if magic == 5:
+    print("Ask again later.")
+
+if magic == 6:
+    print("Better not tell you now.")
+
+if magic == 7:
+    print("Cannot predict now.")
+
+if magic == 8:
+    print("Concentrate and ask again.")
+
+if magic == 9:
+    print("Don’t count on it.")
+
+if magic == 10:
+    print("It is certain.")
+ 
+if magic == 11:
+    print("It is decidedly so.")
+ 
+if magic == 12:
+    print("Most likely.")
+
+if magic == 13:
+    print("My reply is no.")
+
+if magic == 14:
+    print("My sources say no.")
+
+if magic == 15:
+    print("Yes – definitely.")
+
+if magic == 16:
+    print("Very doubtful.")
+
+if magic == 17:
+    print("Without a doubt.")
+
+if magic == 18:
+    print("Signs point to yes.")
+
+if magic == 19:
+    print("Reply hazy, try again.")
+
+if magic == 20:
+    print("Outlook good.")
+ 
+ 
+ 
+ 
+ 
+ 
+ 
